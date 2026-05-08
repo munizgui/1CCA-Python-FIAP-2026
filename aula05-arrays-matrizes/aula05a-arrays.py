4e8486d604de9d4520e41e372ba4cbb993cfc457
munizgui <guilhermemuniz181981@gmail.com>
2026-05-08 10:56:54 -0300

    aula 5

---
+++ aula05-arrays-matrizes/aula05a-arrays.py
@@ -0,0 +1,27 @@
+lista_frutas = ['Maçã', 'Morango', 'Uva']
+
+#lista_frutas[0] = 'Maçã'
+#lista_frutas[1] = 'Morango'
+#lista_frutas[2] = 'Uva'
+print(lista_frutas[1])
+
+lista_frutas.append('Jabuticaba')
+print(lista_frutas[-1])
+print()
+
+tamanho = len(lista_frutas)
+
+for i in range(tamanho):
+    print(lista_frutas[i])
+
+print()
+
+for fruta in lista_frutas:
+    print(fruta)
+
+print()
+
+msg = 'Oi fulano!'
+
+for i in range(len(msg)):
+    print(msg[i])
---
+++ aula05-arrays-matrizes/desafio-01.py
@@ -0,0 +1,5 @@
+lista_nomes = ['Bob', 'luis', 'ana', 'jo']
+
+for i in range(len(lista_nomes)):
+    for j in range(i):
+        print(lista_nomes[i],lista_nomes[j])
